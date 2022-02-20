637442b26a76841d42f4785774b5fbf30dec92d9
Tim Harding <tim@timharding.co>
2022-02-19 19:39:11 -0800

    Revert "Printing hash in little endian"
    Turns out the old way was correct
    
    This reverts commit 5ff45aa2f0e63b99325ef6c32550276f140596f8.

--- digest.c
+++ digest.c
@@ -8,16 +8,6 @@ bool Digest_equal(Digest left, Digest right) {
         left.d == right.d;
 }
 
-void print_little_endian(u32 n) {
-    u8* bytes = (u8*) &n;
-    for (int i = 0; i < 4; i++) {
-        printf("%02x", bytes[i]);
-    }
-}
-
 void Digest_print(Digest digest) {
-    print_little_endian(digest.a);
-    print_little_endian(digest.b);
-    print_little_endian(digest.c);
-    print_little_endian(digest.d);
+    printf("%x%x%x%x", digest.a, digest.b, digest.c, digest.d);
 }
