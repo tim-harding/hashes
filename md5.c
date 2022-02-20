f8857bb079bd44e2d05548296945335ca3010987
Tim Harding <tim@timharding.co>
2022-02-19 20:33:53 -0800

    Back to previous implementation

--- md5.c
+++ md5.c
@@ -34,52 +34,29 @@ Digest hash(const char* message) {
         u32 c = c0;
         u32 d = d0;
 
-        for (u32 i = 0; i < 64; i++) {
-            u32 f, g;
-            if (i < 16) {
-                f = (b & c) | (~b & d);
-                g = i;
-            } else if (i < 32) {
-                f = (d & b) | (~d & c);
-                g = (5 * i + 1) % 16;
-            } else if (i < 48) {
-                f = b ^ c ^ d;
-                g = (3 * i + 5) % 16;
-            } else {
-                f = c ^ (b | ~d);
-                g = (7 * i) % 16;
-            }
-
-            f += a + SINES[i] + (*block)[g];
-            a = d;
-            d = c;
-            c = b;
-            b += rotate_left(f, SHIFTS[i]);
+        for (u32 i = 0; i < 16; i++) {
+            u32 f = (b & c) | (~b & d);
+            u32 g = i;
+            shuffle(block, &a, &b, &c, &d, i, f, g);
         }
 
-        // for (u32 i = 0; i < 16; i++) {
-        //     u32 f = (b & c) | (~b & d);
-        //     u32 g = i;
-        //     shuffle(block, &a, &b, &c, &d, i, f, g);
-        // }
+        for (u32 i = 16; i < 32; i++) {
+            u32 f = (d & b) | (~d & c);
+            u32 g = (5 * i + 1) % 16;
+            shuffle(block, &a, &b, &c, &d, i, f, g);
+        }
 
-        // for (u32 i = 16; i < 32; i++) {
-        //     u32 f = (d & b) | (~d & c);
-        //     u32 g = (5 * i + 1) % 16;
-        //     shuffle(block, &a, &b, &c, &d, i, f, g);
-        // }
+        for (u32 i = 32; i < 48; i++) {
+            u32 f = b ^ c ^ d;
+            u32 g = (3 * i + 5) % 16;
+            shuffle(block, &a, &b, &c, &d, i, f, g);
+        }
 
-        // for (u32 i = 32; i < 48; i++) {
-        //     u32 f = b ^ c ^ d;
-        //     u32 g = (3 * i + 5) % 16;
-        //     shuffle(block, &a, &b, &c, &d, i, f, g);
-        // }
-
-        // for (u32 i = 48; i < 64; i++) {
-        //     u32 f = c ^ (b | ~d);
-        //     u32 g = (7 * i) % 16;
-        //     shuffle(block, &a, &b, &c, &d, i, f, g);
-        // }
+        for (u32 i = 48; i < 64; i++) {
+            u32 f = c ^ (b | ~d);
+            u32 g = (7 * i) % 16;
+            shuffle(block, &a, &b, &c, &d, i, f, g);
+        }
 
         a0 += a;
         b0 += b;
