04d3025320266aabbd15ebba55fecb1650382af3
Tim Harding <tim@timharding.co>
2022-02-24 12:13:44 -0800

    Made shuffle a pure function

--- md5.c
+++ md5.c
@@ -6,12 +6,13 @@ u32 rotate_left(u32 n, u8 bits) {
     return (n << bits) | (n >> (32 - bits));
 }
 
-void shuffle(Block* block, u32* a, u32* b, u32* c, u32* d, u32 i, u32 f, u32 g) {
-    f += *a + SINES[i] + block->word[g];
-    *a = *d;
-    *d = *c;
-    *c = *b;
-    *b += rotate_left(f, SHIFTS[i]);
+Digest shuffle(Block* block, Digest digest, u32 i, u32 f, u32 g) {
+    f += digest.a + SINES[i] + block->word[g];
+    digest.a = digest.d;
+    digest.d = digest.c;
+    digest.c = digest.b;
+    digest.b += rotate_left(f, SHIFTS[i]);
+    return digest;
 }
 
 // Message will be reallocated with padding for now.
@@ -31,39 +32,36 @@ Digest hash(const char* message) {
     for (u32 block_i = 0; block_i < padded.block_count; block_i++) {
         Block* block = &padded.blocks[block_i];
 
-        u32 a = digest.a;
-        u32 b = digest.b;
-        u32 c = digest.c;
-        u32 d = digest.d;
+        Digest inner = digest;
 
         for (u32 i = 0; i < 16; i++) {
-            u32 f = (b & c) | (~b & d);
+            u32 f = (inner.b & inner.c) | (~inner.b & inner.d);
             u32 g = i;
-            shuffle(block, &a, &b, &c, &d, i, f, g);
+            inner = shuffle(block, inner, i, f, g);
         }
 
         for (u32 i = 16; i < 32; i++) {
-            u32 f = (d & b) | (~d & c);
+            u32 f = (inner.d & inner.b) | (~inner.d & inner.c);
             u32 g = (5 * i + 1) % 16;
-            shuffle(block, &a, &b, &c, &d, i, f, g);
+            inner = shuffle(block, inner, i, f, g);
         }
 
         for (u32 i = 32; i < 48; i++) {
-            u32 f = b ^ c ^ d;
+            u32 f = inner.b ^ inner.c ^ inner.d;
             u32 g = (3 * i + 5) % 16;
-            shuffle(block, &a, &b, &c, &d, i, f, g);
+            inner = shuffle(block, inner, i, f, g);
         }
 
         for (u32 i = 48; i < 64; i++) {
-            u32 f = c ^ (b | ~d);
+            u32 f = inner.c ^ (inner.b | ~inner.d);
             u32 g = (7 * i) % 16;
-            shuffle(block, &a, &b, &c, &d, i, f, g);
+            inner = shuffle(block, inner, i, f, g);
         }
 
-        digest.a += a;
-        digest.b += b;
-        digest.c += c;
-        digest.d += d;
+        digest.a += inner.a;
+        digest.b += inner.b;
+        digest.c += inner.c;
+        digest.d += inner.d;
     }
 
     PaddedMessage_free(&padded);
