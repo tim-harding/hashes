48cec3a2813595c591d6c5758f96aadcc633cd58
Tim Harding <tim@timharding.co>
2022-03-06 11:24:33 -0800

    Splitting out MD5 and SHA1 implementations

---
+++ sha1/sha1.c
@@ -0,0 +1,81 @@
+#include "sha1.h"
+
+u32 rotate_left(u32 n, u8 bits) { return (n << bits) | (n >> (32 - bits)); }
+
+Digest shuffle(Block* block, Digest digest, u32 i, u32 f, u32 g) {
+    f += digest.a + SINES[i] + block->word[g];
+    digest.a = digest.d;
+    digest.d = digest.c;
+    digest.c = digest.b;
+    u32 shifts_index = (i / 16) * 4 + (i % 4);
+    digest.b += rotate_left(f, SHIFTS[shifts_index]);
+    return digest;
+}
+
+Digest contribute_block(Block* block, Digest digest) {
+    Digest inner = digest;
+
+    for (u32 i = 0; i < 16; i++) {
+        u32 f = (inner.b & inner.c) | (~inner.b & inner.d);
+        u32 g = i;
+        inner = shuffle(block, inner, i, f, g);
+    }
+
+    for (u32 i = 16; i < 32; i++) {
+        u32 f = (inner.d & inner.b) | (~inner.d & inner.c);
+        u32 g = (5 * i + 1) % 16;
+        inner = shuffle(block, inner, i, f, g);
+    }
+
+    for (u32 i = 32; i < 48; i++) {
+        u32 f = inner.b ^ inner.c ^ inner.d;
+        u32 g = (3 * i + 5) % 16;
+        inner = shuffle(block, inner, i, f, g);
+    }
+
+    for (u32 i = 48; i < 64; i++) {
+        u32 f = inner.c ^ (inner.b | ~inner.d);
+        u32 g = (7 * i) % 16;
+        inner = shuffle(block, inner, i, f, g);
+    }
+
+    return Digest_sum(digest, inner);
+}
+
+void initialize_sines() {
+    for (u32 i = 0; i < BLOCK_BYTES; i++) {
+        SINES[i] = floor(SINE_MULTIPLICAND * fabs(sin(i + 1)));
+    }
+}
+
+Digest hash(const char* message) {
+    initialize_sines();
+    Block* blocks = (Block*)message;
+    u32 message_length = strlen(message);
+    u32 whole_block_count = message_length / BLOCK_BYTES;
+    u32 last_block_remainder = message_length % BLOCK_BYTES;
+
+    Digest digest = {
+        .a = 0x67452301,
+        .b = 0xefcdab89,
+        .c = 0x98badcfe,
+        .d = 0x10325476,
+    };
+
+    for (u32 block_i = 0; block_i < whole_block_count; block_i++) {
+        digest = contribute_block(&blocks[block_i], digest);
+    }
+
+    Block padded_block;
+    memset(&padded_block, 0, BLOCK_BYTES);
+    memcpy(&padded_block, &blocks[whole_block_count], last_block_remainder);
+    padded_block.byte[last_block_remainder] = 0x80;
+    if (last_block_remainder > FINAL_BLOCK_MESSAGE_BYTES) {
+        digest = contribute_block(&padded_block, digest);
+        memset(&padded_block.byte, 0, BLOCK_BYTES);
+    }
+    padded_block.long_word[BLOCK_U64S - 1] = message_length * BITS_PER_BYTE;
+    digest = contribute_block(&padded_block, digest);
+
+    return digest;
+}
---
+++ sha1/sha1.h
@@ -0,0 +1,49 @@
+#ifndef SHA1_H
+#define SHA1_H
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "digest.h"
+#include "th_stdlib.h"
+
+#define BITS_PER_BYTE 8
+#define BLOCK_BITS 512
+#define BLOCK_BYTES (BLOCK_BITS / BITS_PER_BYTE)
+#define BLOCK_U32S (BLOCK_BITS / 32)
+#define BLOCK_U64S (BLOCK_BITS / 64)
+
+// The message is postpended by a 64-bit number
+// that gives the original message length.
+#define MESSAGE_LENGTH_BYTES 8
+
+// The message must be postpended by 0x80,
+// which takes one byte.
+#define MINIMUM_PADDING_BYTES 1
+
+#define FINAL_BLOCK_MESSAGE_BYTES \
+    (BLOCK_BYTES - MESSAGE_LENGTH_BYTES - MINIMUM_PADDING_BYTES)
+
+#define SINE_MULTIPLICAND ((u64)1 << (u64)32)
+
+typedef union Block {
+    u8 byte[BLOCK_BYTES];
+    u32 word[BLOCK_U32S];
+    u64 long_word[BLOCK_U64S];
+} Block;
+
+const u8 SHIFTS[] = {
+    7, 12, 17, 22, // Round 1
+    5, 9,  14, 20, // Round 2
+    4, 11, 16, 23, // Round 3
+    6, 10, 15, 21, // Round 4
+};
+
+u32 SINES[64];
+
+Digest hash(const char* message);
+
+#endif
